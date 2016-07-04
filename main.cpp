dbb1a69d120717c1384e0da79dc565f04a7662f8
polinom2686 <polinom2686@gmail.com>
2016-07-04 12:59:44 +0300

    Предрелизная версия

--- main.cpp
+++ main.cpp
@@ -29,22 +29,22 @@ const string XML_FIXend = "</fix>";
 
 struct gpsdata
 {
-    float   lat;
-    float   lon;
-    float   ele;
-    int64_t utc;
-    float   hdop;
-    float   vdop;
-    int     sat;
-    string  fix;
+    string   lat;
+    string   lon;
+    string   ele;
+    string   utc;
+    string   hdop;
+    string   vdop;
+    string   sat;
+    string   fix;
 };
 
-string getParam(string& sdata, string sbeg, string send, size_t& curpos)
+string getParam(string& sdata, string sbeg, string send)
 {
     size_t pbeg = 0;
     size_t pend = 0;
 
-    pbeg = sdata.find(sbeg, curpos);
+    pbeg = sdata.find(sbeg);
     if (pbeg == string::npos)
     {
         return "";
@@ -54,9 +54,6 @@ string getParam(string& sdata, string sbeg, string send, size_t& curpos)
     pend = sdata.find(send, pbeg);
 
     string temp = sdata.substr(pbeg, pend - pbeg);
-
-    curpos += pend;
-    curpos += send.size();
 
     return temp;
 }
@@ -94,6 +91,18 @@ time_t tm_to_time_t_utc( struct tm * timeptr ) {
   and then adds the appropriate number of seconds to make it UTC */
   return mktime( timeptr ) + get_utc_offset() * 3600;
 
+}
+
+string query_insert(gpsdata& data)
+{
+    string temp;
+    
+    temp += "insert into points (UTC, LAT, LON, ELE, SAT, HDOP, VDOP, FIX) ";
+    temp += "values (";
+    temp += data.utc + ", " + data.lat + ", " + data.lon + ", " + data.ele + ", " +
+            data.sat + ", " + data.hdop + ", " + data.vdop + ", '" + data.fix + "');";
+    
+    return temp;
 }
 
 int main(int argc, char* argv[])
@@ -142,57 +151,93 @@ int main(int argc, char* argv[])
         return 4;
     }
 
-
-    fs.read((char*)fileData.get(), fileSize);
-
-    string sdata((char*)fileData.get(), fileSize);
-
-    size_t curPos = 0;
-
-
+    string sdata;
     vector<gpsdata> gps;
     gps.clear();
 
-    while (true)
+    while (getline(fs,sdata))
     {
         gpsdata dtemp;
         string  stemp;
 
         // lat lon ele time hdop vdop sat fix
 
-        stemp = getParam(sdata,XML_LATbeg,XML_LATend,curPos);
+        stemp = getParam(sdata,XML_LATbeg,XML_LATend);
         if (0 == stemp.size())
         {
-            break;
+            continue;
         }
-        dtemp.lat = stof(stemp);
-        #ifdef DEBUG
-
-        cout << dtemp.lat << endl;
-
-        #endif // DEBUG
-
-        dtemp.lon = stof(getParam(sdata,XML_LONbeg,XML_LONend,curPos));
-        dtemp.ele = stof(getParam(sdata,XML_ELEbeg,XML_ELEend,curPos));
-        stemp = getParam(sdata,XML_TIMEbeg,XML_TIMEend,curPos);
+        dtemp.lat = stemp;
+        dtemp.lon = getParam(sdata,XML_LONbeg,XML_LONend);
+        dtemp.ele = getParam(sdata,XML_ELEbeg,XML_ELEend);
+        stemp = getParam(sdata,XML_TIMEbeg,XML_TIMEend);
         struct tm t;
-        t.tm_year = stoi(stemp.substr(0,4));
+        t.tm_year = stoi(stemp.substr(0,4)) - 1900;
         t.tm_mon = stoi(stemp.substr(5,2)) - 1;
         t.tm_mday = stoi(stemp.substr(8,2));
         t.tm_hour = stoi(stemp.substr(11,2));
         t.tm_min = stoi(stemp.substr(14,2));
         t.tm_sec = stoi(stemp.substr(17,2));
-        dtemp.utc = tm_to_time_t_utc(&t);
-        dtemp.hdop = stof(getParam(sdata,XML_HDOPbeg,XML_HDOPend,curPos));
-        dtemp.vdop = stof(getParam(sdata,XML_VDOPbeg,XML_VDOPend,curPos));
-        dtemp.sat = stoi(getParam(sdata,XML_SATbeg,XML_SATend,curPos));
-        dtemp.fix = getParam(sdata,XML_FIXbeg,XML_FIXend,curPos);
+        dtemp.utc = to_string(tm_to_time_t_utc(&t));
+        dtemp.hdop = getParam(sdata,XML_HDOPbeg,XML_HDOPend);
+        dtemp.vdop = getParam(sdata,XML_VDOPbeg,XML_VDOPend);
+        dtemp.sat = getParam(sdata,XML_SATbeg,XML_SATend);
+        dtemp.fix = getParam(sdata,XML_FIXbeg,XML_FIXend);
 
         gps.push_back(dtemp);
     }
+    
+    if (gps.empty())
+    {
+        return 5;
+    }
 
-    cout << endl << gps[0].utc << endl;
+    #ifdef DEBUG
+    if (gps.size() > 0)
+    {
+        fstream fsd("debug.info", ios::out);
+        for (size_t i = 0; i < gps.size(); ++i)
+        {
+            // lat lon ele time hdop vdop sat fix
+            fsd << gps[i].lat << "\t"
+                << gps[i].lon << "\t"
+                << gps[i].ele << "\t"
+                << gps[i].utc << "\t"
+                << gps[i].hdop << "\t"
+                << gps[i].vdop << "\t"
+                << gps[i].sat << "\t"
+                << gps[i].fix << endl;
+            fsd << query_insert(gps[i]) << endl;
+        }
+        fsd.close();
+        return 0;
+    }
+    #endif // DEBUG
+    
+    /* Insert data to DB
+    */
 
+    nanodbc::connection conn;
+    
+    try
+    {
+        conn.connect(dsnName,dsnUser,dsnPass);
+    }
+    catch(nanodbc::database_error dbe)
+    {
+        cout << dbe.what() << endl;
+    }
+    
+    for (size_t i = 0; i < gps.size(); ++i)
+    {
+        string qtemp;
+        
+        qtemp = query_insert(gps[i]);
+        
+        nanodbc::just_execute(conn, qtemp);
+    }
+    
+    conn.disconnect();
 
     return 0;
 }
